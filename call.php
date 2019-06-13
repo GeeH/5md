46cb9589e93a5a6dfb316f50ca476fb4ad19240f
Gary Hockin <gary@hock.in>
2019-06-13 10:07:15 +0100

    Updated file stub

--- call.php
+++ call.php
@@ -1,3 +1,8 @@
 <?php declare(strict_types=1);
 
 require_once('vendor/autoload.php');
+
+$twilio = new \Twilio\Rest\Client(
+    'ACb877821242bbaedc246328ca0a8c3fc6',
+    getenv('TWILIO_TOKEN')
+);
